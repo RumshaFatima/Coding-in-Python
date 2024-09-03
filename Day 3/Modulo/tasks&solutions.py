6efdca513015a339a1db7311620377d7684898bc
RumshaFatima <79537254+RumshaFatima@users.noreply.github.com>
2024-09-03 23:49:26 +0500

    Update and rename solution.py to tasks&solutions.py

---
+++ Day 3/Modulo/tasks&solutions.py
@@ -0,0 +1,14 @@
+number_to_check = int(input("What is the number you want to check? "))
+
+if number_to_check % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+
+#......or......
+
+print(number:=int(input("Write any whole number from number series\n")))
+if number%2==0:
+    print(f"{number} is an even number")
+else:
+    print(f"{number} is an odd number")
