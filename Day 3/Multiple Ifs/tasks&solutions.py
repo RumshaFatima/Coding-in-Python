43c267d6e320d23064346ae3f9d6032d0f04ea12
RumshaFatima <79537254+RumshaFatima@users.noreply.github.com>
2024-09-04 00:37:17 +0500

    Update and rename solution.py to tasks&solutions.py

--- Day 3/Multiple Ifs/tasks&solutions.py
+++ Day 3/Multiple Ifs/tasks&solutions.py
@@ -15,7 +15,7 @@ if height >= 120:
         bill = 12
         print("Adult tickets are $12.")
 
-    wants_photo = input("Do you want a photo taken? Y or N. ")
+    print(wants_photo := input("Do you want a photo taken? Y or N. \n"))
     if wants_photo == "Y":
         bill += 3
 
