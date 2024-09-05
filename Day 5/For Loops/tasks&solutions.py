76f6cc35dd155a950dc5a05a589c413cbdb37a49
RumshaFatima <79537254+RumshaFatima@users.noreply.github.com>
2024-09-05 09:11:09 +0500

    Update tasks&solutions.py

--- Day 5/For Loops/tasks&solutions.py
+++ Day 5/For Loops/tasks&solutions.py
@@ -32,3 +32,27 @@ for fruit_list in fruits:
     print(pie_fruit:=[f"{fruit} pie" for fruit in fruit_list ])
     print(", ".join(pie_fruit[:-1]) + " & " + pie_fruit[-1])
 print(fruits)
+
+#..............or...............
+
+# Initial list of fruits
+fruits = [["Apple", "Peach", "Pear"], ["Apple", "Peach", "Pear"]]
+
+# Modify the second nested list
+fruits[1][2] = "banana"
+
+# Append a new nested list to the fruits
+fruits.append(["strawberry"])
+
+# Iterate through each nested list
+for fruit_list in fruits:
+    if len(fruit_list) == 1:
+        # Special case: Only one fruit in the list
+        print(f"{fruit_list[0]} pie")
+    else:
+        # Standard case: More than one fruit in the list
+        pie_fruits = [f"{fruit} pie" for fruit in fruit_list]
+        print(", ".join(pie_fruits[:-1]) + " & " + pie_fruits[-1])
+
+# Print the modified fruits list
+print(fruits)
