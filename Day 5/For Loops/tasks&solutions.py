4077ffd44c782bfeeccdd58cc9b6ec76b3243fe0
RumshaFatima <79537254+RumshaFatima@users.noreply.github.com>
2024-09-05 09:05:26 +0500

    Update tasks&solutions.py

--- Day 5/For Loops/tasks&solutions.py
+++ Day 5/For Loops/tasks&solutions.py
@@ -22,3 +22,13 @@ fruits.append("strawberry")
 for fruit in fruits:
     print(f"{fruit[0]} pie, {fruit[1]} pie, {fruit[2]} pie & {fruit}")
 print(fruits)
+
+#............or......................
+
+fruits = [["Apple", "Peach", "Pear"],["Apple", "Peach", "Pear"]]
+fruits[1][2]="banana"
+fruits.append(["strawberry"])
+for fruit_list in fruits:
+    print(pie_fruit:=[f"{fruit} pie" for fruit in fruit_list ])
+    print(", ".join(pie_fruit[:-1]) + " & " + pie_fruit[-1])
+print(fruits)
