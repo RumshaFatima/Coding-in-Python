b706b1a997ff42a3c5b658e3cd192b0e307a6509
RumshaFatima <79537254+RumshaFatima@users.noreply.github.com>
2024-09-05 09:45:28 +0500

    Update and rename solution.py to tasks&solutions.py

--- Day 5/For Loops with Range/tasks&solutions.py
+++ Day 5/For Loops with Range/tasks&solutions.py
@@ -12,3 +12,15 @@ total = 0
 for number in range(1, 101):
     total += number
 print(total)
+
+#Fizz or Buzz
+
+for number in range(1,101):
+    if number%3==0 and number%5==0:
+        print("FizzBuzz")
+    elif number%3==0 and number%5 !=0:
+        print("Fizz")
+    elif number%5==0 and number%3 !=0:
+        print("Buzz")
+    else:
+        print(number)
